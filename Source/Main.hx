68d1f49d5b4f66fce97fa0789fe5e4d659ee64bd
jsuarezb <jsuarezb@outlook.com>
2015-07-11 03:31:51 -0300

    Hey

--- Source/Main.hx
+++ Source/Main.hx
@@ -2,11 +2,14 @@ package;
 
 import openfl.display.Sprite;
 
-class Main extends Sprite {
+class Main extends Sprite
+{
 
-	public function new () {
+	public function new ()
+	{
 		super();
-		
+
+
 	}
 
 }
